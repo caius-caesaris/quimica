d2f69489b93d950046457bd4f68678cc7a70d2a0
caius-caesaris <2025311443@aluno.riogrande.ifrs.edu.br>
2026-08-31 21:01:13 -0300

    .

---
+++ script/main.js
@@ -0,0 +1,86 @@
+import questionsJSON from "./JSON/questions.json" with { type: "json" };
+
+const MAX_QUESTIONS = 5
+
+function shuffle(array) {
+    const copy = [...array]
+    for (let i = copy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[copy[i], copy[j]] = [copy[j], copy[i]]
+    }
+    return copy
+}
+
+const selectedQuestions = shuffle(questionsJSON.questions).slice(0, MAX_QUESTIONS)
+
+let currentIndex = 0
+let corrects = 0
+let correctArray = []
+
+const description = document.querySelector("#question-description")
+const labels = [1, 2, 3, 4].map(i => document.querySelector(`#label${i}`))
+const radios = [1, 2, 3, 4].map(i => document.querySelector(`#alt${i}`))
+const nextButton = document.querySelector("#next-button")
+const postQuiz = document.querySelector("#post-quiz")
+const resetButton = document.querySelector("#reset-button")
+const questionBox = document.querySelector("#question")
+
+function renderQuestion(index) {
+    const question = selectedQuestions[index]
+
+    description.innerHTML = question.description
+
+    question.options.forEach((option, i) => {
+        labels[i].innerHTML = option
+        radios[i].checked = false
+    })
+}
+
+function checkAnswer() {
+    const question = selectedQuestions[currentIndex]
+    const selectedIndex = radios.findIndex(radio => radio.checked)
+
+    if (selectedIndex == -1) {
+        alert("Selecione uma alternativa antes de continuar.") // Alterar depois para HTML
+        return
+    }
+
+    if (selectedIndex == question.correct) {
+        corrects++
+        correctArray.concat(true)
+    } else {
+        correctArray.concat(false)
+    }
+
+    currentIndex++
+
+    if (currentIndex < selectedQuestions.length) {
+        renderQuestion(currentIndex)
+    } else {
+        questionBox.style.display = "none"
+        postQuiz.style.display = "block"
+        resetButton.style.display = "block"
+        postQuiz.innerHTML = `Você acertou ${corrects} de ${selectedQuestions.length} perguntas.`
+
+        correctArray.forEach((answer, i) => {
+            if (answer) {
+                postQuiz.innerHTML += `<br>A questão ${i + 1} está correta`
+            } else {
+                postQuiz.innerHTML += `<br>A questão ${i + 1} está incorreta`
+            }
+        })
+
+        resetButton.addEventListener("click", () => {
+            currentIndex = 0
+            corrects = 0
+            questionBox.style.display = "block"
+            postQuiz.style.display = "none"
+            resetButton.style.display = "none"
+            renderQuestion(currentIndex)
+        })
+    }
+}
+
+nextButton.addEventListener("click", checkAnswer)
+
+renderQuestion(currentIndex)
